8b4235b33391711f3b47b70b91d26a2dc15d4062
shadow-eljan <eljanrai25@gmail.com>
2025-12-31 22:53:32 +0545

    Setting up Servlet

---
+++ src/main/java/com/servlet/HelloServlet.java
@@ -0,0 +1,9 @@
+package com.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+}
