2f75d796d96859c990481b34b199f71108896fda
shadow-eljan <eljanrai25@gmail.com>
2026-01-07 11:40:01 +0545

    Dashboard Servlet

---
+++ src/main/java/com/servlet/Dashboard.java
@@ -0,0 +1,23 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/dashboard")
+public class Dashboard extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String name = (String) req.getAttribute("user");
+
+        out.println("Welcome to dashboard"+ name);
+        out.close();
+    }
+}
