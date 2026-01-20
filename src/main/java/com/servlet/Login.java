93b95a6e0ff3bc32f6bb07c2683e86dacf5ee328
shadow-eljan <eljanrai25@gmail.com>
2026-01-20 07:17:42 +0545

    Login servlet

---
+++ src/main/java/com/servlet/Login.java
@@ -0,0 +1,33 @@
+package com.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/login.html");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+
+        if(name.equalsIgnoreCase("admin")){
+            HttpSession session = req.getSession(true);
+            session.setAttribute("name", name);
+            session.setMaxInactiveInterval(60);
+            resp.sendRedirect("dashboard");
+        }
+    }
+}
